af301bcfa384941cc46d8c1b5387651f002a907b
GerardFritzC <100100532+GerardFritzC@users.noreply.github.com>
2022-04-27 00:21:10 +0800

    Add files via upload

---
+++ LT5/javascript.js
@@ -0,0 +1,97 @@
+alert("World War 2 Famous Leaders.");
+
+function readmore(){
+	var dots = document.getElementById("dots");
+	var moretext = document.getElementById('more');
+	var btn = document.getElementById('btn');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
+
+function readmore2(){
+	var dots = document.getElementById("dots2");
+	var moretext = document.getElementById('more2');
+	var btn = document.getElementById('btn2');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
+
+function readmore3(){
+	var dots = document.getElementById("dots3");
+	var moretext = document.getElementById('more3');
+	var btn = document.getElementById('btn3');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
+
+function readmore4(){
+	var dots = document.getElementById("dots4");
+	var moretext = document.getElementById('more4');
+	var btn = document.getElementById('btn4');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
+
+function readmore5(){
+	var dots = document.getElementById("dots5");
+	var moretext = document.getElementById('more5');
+	var btn = document.getElementById('btn5');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
+
+function readmore6(){
+	var dots = document.getElementById("dots6");
+	var moretext = document.getElementById('more6');
+	var btn = document.getElementById('btn6');
+	
+	if(dots.style.display === "none"){
+	   dots.style.display = "inline";
+	   btn.innerHTML = "Read more";
+	   moretext.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btn.innerHTML = "Read less";
+		moretext.style.display = 'inline';
+	}
+}
